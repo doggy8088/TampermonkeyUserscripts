5b1a4f3f7c1c98ebae4fcfec24e697c8b67477a0
Will <doggy.huang@gmail.com>
2023-03-13 14:39:57 +0800

    SinoBank: 永豐銀行 MMA 登入啟用密碼管理器機制 v1.0.1

--- src/SinoBankMMASignIn.user.js
+++ src/SinoBankMMASignIn.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         SinoBank: 永豐銀行 MMA 登入啟用密碼管理器機制
-// @version      1.0.0
+// @version      1.0.1
 // @description  永豐銀行 MMA 登入啟用密碼管理器機制
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -16,8 +16,13 @@
 (function() {
     'use strict';
 
-    document.querySelector('[placeholder="身分證字號(統一編號)"]').id = 'sid'
-    document.querySelector('[placeholder="使用者代碼"]').id = 'userid'
-    document.querySelector('[placeholder="網路密碼"]').id = 'userpw'
+    let sid = document.querySelector('[placeholder="身分證字號(統一編號)"]');
+    if (sid) sid.id = 'sid'
+
+    let userid = document.querySelector('[placeholder="使用者代碼"]');
+    if (userid) userid.id = 'userid'
+
+    let userpw = document.querySelector('[placeholder="網路密碼"]');
+    if (userpw) userpw.id = 'userpw'
 
 })();
