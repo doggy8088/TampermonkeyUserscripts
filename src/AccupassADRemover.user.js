285c53dbd92359d2c07561c50242ee7cf9f6f5b0
Will <doggy.huang@gmail.com>
2023-12-07 01:49:01 +0800

    chore: improve performance and compatibility across platforms
    - Update version from `1.1` to `1.2`
    - Change @match URL to include all pages on `https://www.accupass.com/`
    - Simplify the CSS rule for removing the floating ad into a single line

--- src/AccupassADRemover.user.js
+++ src/AccupassADRemover.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Accupass: 刪除活動頁面的漂浮廣告
-// @version      1.1
+// @version      1.2
 // @description  刪除 Accupass 前台活動頁的漂浮廣告
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -9,18 +9,14 @@
 // @source       https://github.com/doggy8088/TampermonkeyUserscripts/raw/main/src/AccupassADRemover.user.js
 // @namespace    https://github.com/doggy8088/TampermonkeyUserscripts/raw/main/src/AccupassADRemover.user.js
 // @author       Will Huang
-// @match        https://www.accupass.com/event/*
+// @match        https://www.accupass.com/*
 // @run-at       document-body
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    var css = `
-    div[class*="download-app-container"] {
-        display: none;
-    }
-`;
+    var css = `div[class*="download-app-container"] { display: none; }`;
 
     var style = document.createElement("style");
     style.innerHTML = css
