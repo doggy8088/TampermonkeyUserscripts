e176bc8cbb0134c4aaa5f36b1dcf7b99c42f7e55
Will <doggy.huang@gmail.com>
2024-06-18 23:07:32 +0800

    按下兩次 Ctrl+C 就會自動複製網址 v0.3.0
    支援 github.com 直接複製 git clone 網址

--- src/CtrlCCtrlCCopyURL.user.js
+++ src/CtrlCCtrlCCopyURL.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         按下兩次 Ctrl+C 就會自動複製網址
-// @version      0.2.0
+// @version      0.3.0
 // @description  按下兩次 Ctrl+C 就會自動複製網址，為了方便自行實作複製網址的邏輯，因此這份腳本不會變更版號，請自行檢查是否有新版。
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -18,12 +18,27 @@
     document.addEventListener('copy', function(event) {
         let now = new Date().getTime();
 
+        // 如果使用者正在選取文字，就不要觸發 Ctrl+C 複製網址的功能
+        let isUserSelectingText = window.getSelection().toString().length > 0;
+
         // 只要在 1 秒內連續按兩次 Ctrl+C，就會自動複製網址
-        if (now - lastCopy < 1000 && !window.getSelection().toString()) {
+        if (now - lastCopy < 1000 && !isUserSelectingText) {
             let url = window.location.href;
 
             // 過濾掉 learn.microsoft.com 網站上的 view=aspnetcore- 參數，確保拿到的網址一定是最新版
-            url = url.replace(/view=aspnetcore-\d.\d&/g, '');
+            if (location.host === 'learn.microsoft.com') {
+                url = url.replace(/view=aspnetcore-\d.\d&/g, '');
+            }
+
+            if (location.host === 'github.com') {
+                // url: https://github.com/google-gemini/gemma-cookbook/tree/main
+                const parts = url.split('/');
+                const owner = parts?.[3];
+                const repo = parts?.[4];
+                if (owner && repo) {
+                    url = `https://github.com/${owner}/${repo}.git`;
+                }
+            }
 
             navigator.clipboard.writeText(url)
                 .then(() => {
