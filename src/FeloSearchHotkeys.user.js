de47d65a12e014651af1594cd88727b31a188da3
Will <doggy.huang@gmail.com>
2024-12-25 00:23:56 +0800

    feat(FeloSearchHotkeys): 更新版本至 0.3.0
    - 新增 Alt + j 和 Alt + k 快速切換搜尋歷史記錄功能
    - 新增 Alt + / 快速進入歷史記錄頁面
    - 新增 Alt + t 快速進入主題集頁面
    - 新增 Alt + s 快速點擊「分享」按鈕
    - 優化 Ctrl + Delete 刪除邏輯
    - 加入 Escape 快速鍵
      - 不在輸入框就直接回首頁
      - 在輸入框就清空輸入框
      - 在輸入框且在歷史記錄搜尋輸入框沒有資料就回上一頁

--- src/FeloSearchHotkeys.user.js
+++ src/FeloSearchHotkeys.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Felo Search: 好用的鍵盤快速鍵集合
-// @version      0.2.0
+// @version      0.3.0
 // @description  按下 Ctrl+Delete 快速刪除當下聊天記錄、按下 Ctrl+B 快速切換側邊欄、按下 j 與 k 快速切換搜尋結果頁面
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -16,11 +16,26 @@
 (async function () {
     'use strict';
 
+    // 共用函式：根據按鈕文字內容點擊按鈕
+    function clickButtonByText(buttonTexts) {
+        const buttons = document.querySelectorAll('button');
+        for (let i = 0; i < buttons.length; i++) {
+            const button = buttons[i];
+            if (buttonTexts.includes(button.textContent.trim())) {
+                button.click();
+                break;
+            }
+        }
+    }
+
     document.addEventListener("keydown", async (event) => {
 
+        // 從網址列取得 pathinfo
+        const currentPath = window.location.pathname;
+
         if (event.key === "j") {
-            // 如果是輸入欄位，就不要觸發
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+            // 如果是輸入欄位，就不要觸發。但是按下 alt+j 就可以觸發這個功能。
+            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
                 return;
             }
 
@@ -38,18 +53,15 @@
                     nextLink.click();
                 }
             } else {
-                const firstLink = document.querySelector(`a[href^="/search/"]`)
-                    || document.querySelector(`a[href^="/en/search/"]`)
-                    || document.querySelector(`a[href^="/ja/search/"]`)
-                    || document.querySelector(`a[href^="/zh-Hant/search/"]`)
-                    || document.querySelector(`a[href^="/zh-Hans/search/"]`)
+                const firstLink = document.querySelector(`a[href*="/search/"]`);
                 firstLink?.click();
             }
+            event.preventDefault();
         }
 
         if (event.key === "k") {
-            // 如果是輸入欄位，就不要觸發
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+            // 如果是輸入欄位，就不要觸發。但是按下 alt+k 就可以觸發這個功能。
+            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
                 return;
             }
 
@@ -58,69 +70,93 @@
 
             // 2. 找到和目前 pathinfo 一樣的超連結
             const matchingLink = document.querySelector(`a[href="${currentUrl}"]`);
-            if (matchingLink) {
-                // 找到下一個 link
-                const previousLink = matchingLink?.closest('li')?.previousElementSibling?.querySelector('a')
-                    ?? matchingLink?.closest('li')?.previousElementSibling?.previousElementSibling?.querySelector('a');
-                if (previousLink) {
-                    previousLink.parentElement.previousElementSibling.scrollIntoView();
-                    previousLink.click();
-                }
+            const previousLink = matchingLink?.closest('li')?.previousElementSibling?.querySelector('a')
+                ?? matchingLink?.closest('li')?.previousElementSibling?.previousElementSibling?.querySelector('a');
+            if (previousLink) {
+                previousLink.parentElement.previousElementSibling.scrollIntoView();
+                previousLink.click();
             }
+            event.preventDefault();
         }
 
         // 按下 Ctrl+Delete 快速刪除 Felo Search 聊天記錄
         if (event.ctrlKey && event.key === "Delete") {
-            console.log("Ctrl + Delete detected. Starting delete process...");
-
-            // 1. 從網址列取得 pathinfo
-            const currentUrl = window.location.pathname;
-
-            // 2. 找到和目前 pathinfo 一樣的超連結
-            const matchingLink = document.querySelector(`a[href="${currentUrl}"]`);
-            if (matchingLink) {
-
-                // 找到下一個 link
-                const nextLink = matchingLink.closest('li').nextElementSibling.querySelector('a')
-                    ?? matchingLink.closest('li').nextElementSibling.nextElementSibling.querySelector('a');
-
-                // 找到超連結的下一個同層的 section 元素
-                const nextSection = matchingLink.nextElementSibling;
-                // 找到下一層的 button 按鈕
-                const button = nextSection.querySelector("button");
-                button?.click();
-
-                await delay(200); // 加入一點延遲來模擬真實打字過程
-
-                // 找到一個按鈕其內容為「確認」，用迴圈去跑，找出 textContent 為「確認」的按鈕
-                const confirmButton = document.querySelectorAll('button');
-                confirmButton.forEach(async (button) => {
-                    if (button.textContent.trim() === "確認") {
-                        button.click();
-
-                        // 刪除後點擊到下一個連結
-                        await delay(200);
-                        nextLink?.click();
-                    }
-                });
-
-            } else {
-                console.error("找不到符合條件的超連結");
+            // 如果是輸入欄位，就不要觸發
+            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+                return;
             }
+
+            // 找到和目前 pathinfo 一樣的超連結
+            const matchingLink = document.querySelector(`a[href="${currentPath}"]`);
+            const nextLink = matchingLink?.closest('li').nextElementSibling.querySelector('a')
+                ?? matchingLink?.closest('li').nextElementSibling.nextElementSibling.querySelector('a');
+
+            // 找到超連結的下一個同層的 section 元素
+            const nextSection = matchingLink?.nextElementSibling;
+            // 找到下一層的 button 按鈕
+            const button = nextSection.querySelector("button");
+            button?.click();
+
+            await delay(200); // 加入一點延遲來模擬真實打字過程
+
+            // 找到一個按鈕其內容為「確認」，用迴圈去跑，找出 textContent 為「確認」的按鈕
+            const confirmButton = document.querySelectorAll('button');
+            confirmButton.forEach(async (button) => {
+                if (button.textContent.trim() === "確認") {
+                    button.click();
+
+                    // 刪除後點擊到下一個連結
+                    await delay(200);
+                    nextLink?.click();
+                }
+            });
+            event.preventDefault();
         }
 
         // 按下 Ctrl+B 快速切換側邊欄
         if (event.ctrlKey && event.key === "b") {
-            console.log("Ctrl + B detected. Clicking close sidebar button...");
-
             // 找到 section 的 class 為「cursor-pointer」的元素
             const svg = document.querySelector('section.cursor-pointer svg');
-            if (svg) {
-                svg.parentElement.click();
-            } else {
-                console.error("找不到切換側邊欄的按鈕");
-            }
+            svg?.parentElement.click();
+            event.preventDefault();
         }
+
+        // 按下 Escape 就點擊 document.querySelector('img').click()
+        if (event.key === "Escape") {
+            // 如果是輸入欄位，就不要觸發
+            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+                if (event.target.value === '' && currentPath.includes("/history")) {
+                    // 只有在歷史紀錄頁面且搜尋欄位是空白時才會觸發
+                    window.history.back();
+                }
+                if (event.target.value !== '') {
+                    event.target.value = '';
+                }
+                return;
+            }
+            // 找到第一個 img 元素並點擊 (Felo Logo)
+            document.querySelector('img')?.click();
+            event.preventDefault();
+        }
+
+        // 按下 Alt+/ 就先找出所有 button 元素，比對元素內容，如果為「歷史紀錄」就點擊它
+        if (event.altKey && event.key === "/") {
+            clickButtonByText(["歷史記錄", "历史记录", "履歴記録", "History"]);
+            event.preventDefault();
+        }
+
+        // 按下 Alt+t 就先找出所有 button 元素，比對元素內容，如果為「主題集」就點擊它
+        if (event.altKey && event.key === "t") {
+            clickButtonByText(["主題集", "主题集", "トピック集", "Topic Collections"]);
+            event.preventDefault();
+        }
+
+        // 按下 Alt+s 就先找出所有 button 元素，比對元素內容，如果為「分享」就點擊它
+        if (event.altKey && event.key === "s") {
+            clickButtonByText(["分享", "分享", "共有する", "Share"]);
+            event.preventDefault();
+        }
+
     });
 
     // 延遲函式
