be587c61abcaac3e2481b1c14fb3d6f94a8ee053
Will <doggy.huang@gmail.com>
2024-12-29 02:37:32 +0800

    feat(AzureDevOpsPageTitleModifier): 更新版本至 2.0
    - 重構程式碼
    - 加入 wiki 頁面標題修改 (可搭配 espanso 使用)

--- src/AzureDevOpsPageTitleModifier.user.js
+++ src/AzureDevOpsPageTitleModifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Azure DevOps: 調整工作項目頁面標題
-// @version      1.0
-// @description  讓 Azure Boards 的 Work Item 顯示的「頁面標題」更加具有意義
+// @name         Azure DevOps: 調整頁面標題
+// @version      2.0
+// @description  讓 Azure DevOps Service 的「頁面標題」更加具有意義
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
 // @homepageURL  https://blog.miniasp.com/
@@ -12,6 +12,7 @@
 // @match        *://dev.azure.com/*
 // @author       Will Huang
 // @run-at       document-idle
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dev.azure.com
 // ==/UserScript==
 
 // https://www.tampermonkey.net/documentation.php#_run_at
@@ -23,45 +24,69 @@
 
     function executeActions() {
 
-        // https://orgname.visualstudio.com/MyProject/_backlogs/backlog/MyProject%20Team/Backlog%20items/?workitem=30909
-        var searchParams = new URLSearchParams(window.location.search);
-        if (!searchParams.has('workitem')) {
-            return;
+        let pathname = location.pathname;
+        var searchParams = new URLSearchParams(location.search);
+
+        // https://dev.azure.com/org-name/project-name/_workitems/edit/2895
+        // https://dev.azure.com/org-name/project-name/_workitems/edit/2895/
+        // 如果 window.location 有包含 _workitems/edit/ 的字串，表示是 Work Item 的編輯頁面
+        if (location.pathname.includes('_workitems/edit/')) {
+            if (pathname.endsWith('/')) { // 刪除最後一個斜線符號 (/)
+                pathname = pathname.substring(0, pathname.length - 1);
+            }
+            // 取得網址列 pathname 的最後一個字串，例如 2895
+            let id = pathname.substring(pathname.lastIndexOf('/') + 1);
+            updateTitleForWorkItemPage(id);
         }
 
-        // 30909
-        var id = document.querySelector('span[aria-label="ID Field"]')?.innerText;
-        id = searchParams.get('workitem');
+        // 開啟 Work Item 的 Modal dialog 頁面時，網址列上會有 workitem 參數
+        // https://org-name.visualstudio.com/MyProject/_backlogs/backlog/MyProject%20Team/Backlog%20items/?workitem=30909
+        if (searchParams.has('workitem')) {
+            let id = searchParams.get('workitem');
+            updateTitleForWorkItemPage(id);
+        }
 
-        var caption = document.querySelector('[class="caption"]')?.innerText.replace('PRODUCT BACKLOG ITEM', 'PBI');
-        var title = document.querySelector('input[aria-label="Title Field"]')?.value;
+        // 如果 window.location 有包含 /_wiki/ 的字串，表示是 Wiki 頁面
+        // https://miniasp.visualstudio.com/coolrare-training-portal/_wiki/wikis/coolrare-training-portal.wiki/5330/%E5%B0%88%E6%A1%88%E6%91%98%E8%A6%81%E8%B3%87%E8%A8%8A
+        if (pathname.includes('/_wiki/')) {
+            updateTitleForWikiPage();
+        }
 
-        // ["", "MyProject", "_backlogs", "backlog", "MyProject%20Team", "Backlog%20items", ""]
-        var pathnameParts = window.location.pathname.split('/');
-        // "MyProject"
-        var projectName = pathnameParts[1];
-        // "_backlogs"
-        var projectTopPath = pathnameParts[2];
-        // "backlog",
-        var projectSubPath = pathnameParts[3];
-        // "MyProject Team"
-        var projectTeamName = unescape(pathnameParts[4]);
-        // "Backlog items"
-        var projectItemType = unescape(pathnameParts[5]);
+    }
 
-        if (id && caption && title && document.title.indexOf(`${caption}: ${title}`) == -1) {
-            document.title = `${caption}: ${title} - ${projectTeamName}`;
-            // if (intervalId) {
-            //     clearInterval(intervalId);
-            // }
+    function updateTitleForWikiPage() {
+        let headingDiv = document.querySelector('div[role="heading"]');
+        let titleField = document.querySelector('input[aria-label="Page title"]');
+        let title = '';
+        if (!!headingDiv) title = headingDiv?.textContent;
+        if (!!titleField) title = `Editing ${titleField?.value}`;
+        if (!!title) {
+            document.title = `${title} - Wiki - Overview`;
+        }
+    }
+
+    function updateTitleForWorkItemPage(id) {
+        let titleField = document.querySelector('input[aria-label="Title field"]');
+        let title = titleField?.value;
+        let typeSpan = titleField?.parentElement?.parentElement?.parentElement?.parentElement?.previousElementSibling?.querySelector('span[aria-label]')
+        let workItemType = typeSpan?.attributes['aria-label']?.value;
+
+        if (workItemType === 'Product Backlog Item') {
+            workItemType = 'PBI';
+        }
+
+        // let showTeamProfileBtn = document.querySelector('button[aria-label="Show Team Profile"]');
+        // let teamName = showTeamProfileBtn?.parentElement?.previousElementSibling?.textContent;
+
+        // 每個變數都有才更新
+        if (id && title && workItemType) {
+            document.title = `${workItemType} ${id}: ${title}`;
         }
     }
 
     (function () {
         'use strict';
-
         intervalId = setInterval(executeActions, 500);
-
     })();
 
 })();
