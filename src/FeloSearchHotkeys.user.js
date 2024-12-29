f5107e9db7a83e0f41c6539e1e7e6bc91ba97347
Will <doggy.huang@gmail.com>
2024-12-29 15:51:14 +0800

    feat(FeloSearchHotkeys): 更新版本至 0.6.0，優化鍵盤快速鍵邏輯並修正按鈕類型

--- src/FeloSearchHotkeys.user.js
+++ src/FeloSearchHotkeys.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Felo Search: 好用的鍵盤快速鍵集合
-// @version      0.4.0
+// @version      0.6.0
 // @description  按下 Ctrl+Delete 快速刪除當下聊天記錄、按下 Ctrl+B 快速切換側邊欄、按下 j 與 k 快速切換搜尋結果頁面
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -28,6 +28,15 @@
         }
     }
 
+    // SPA 網頁不應該要有 type="submit" 的按鈕，這會導致鍵盤的 Escape 無法被觸發！
+    // setInterval(() => {
+    //     let backdropBlur = document.querySelector('div.backdrop-blur-md');
+    //     let btn = backdropBlur?.parentElement?.querySelector('button');
+    //     if (btn && (btn.attributes['type'] === undefined)) {
+    //         btn.type = 'button';
+    //     }
+    // }, 66);
+
     document.addEventListener("keydown", async (event) => {
 
         // 從網址列取得 pathinfo
@@ -35,7 +44,7 @@
 
         if (event.key === "j") {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+j 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA" || event.target.isContentEditable) && !event.altKey) {
+            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
                 return;
             }
 
@@ -49,8 +58,8 @@
                 const nextLink = matchingLink?.closest('li')?.nextElementSibling?.querySelector('a')
                     ?? matchingLink?.closest('li')?.nextElementSibling?.nextElementSibling?.querySelector('a');
                 if (nextLink) {
-                    nextLink.parentElement.previousElementSibling.scrollIntoView();
-                    nextLink.click();
+                    nextLink?.parentElement?.previousElementSibling?.scrollIntoView();
+                    nextLink?.click();
                 }
             } else {
                 const firstLink = document.querySelector(`a[href*="/search/"]`);
@@ -61,7 +70,7 @@
 
         if (event.key === "k") {
             // 如果是輸入欄位，就不要觸發。但是按下 alt+k 就可以觸發這個功能。
-            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA" || event.target.isContentEditable) && !event.altKey) {
+            if ((event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") && !event.altKey) {
                 return;
             }
 
@@ -82,7 +91,7 @@
         // 按下 Ctrl+Delete 快速刪除 Felo Search 聊天記錄
         if (event.ctrlKey && event.key === "Delete") {
             // 如果是輸入欄位，就不要觸發
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA" || event.target.isContentEditable) {
+            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
                 return;
             }
 
@@ -94,22 +103,15 @@
             // 找到超連結的下一個同層的 section 元素
             const nextSection = matchingLink?.nextElementSibling;
             // 找到下一層的 button 按鈕
-            const button = nextSection.querySelector("button");
+            const button = nextSection?.querySelector("button");
             button?.click();
 
             await delay(200); // 加入一點延遲來模擬真實打字過程
+            clickButtonByText(["確認", "确认", "確認", "Confirm"]);
 
-            // 找到一個按鈕其內容為「確認」，用迴圈去跑，找出 textContent 為「確認」的按鈕
-            const confirmButton = document.querySelectorAll('button');
-            confirmButton.forEach(async (button) => {
-                if (button.textContent.trim() === "確認") {
-                    button.click();
+            await delay(200);
+            nextLink?.click();
 
-                    // 刪除後點擊到下一個連結
-                    await delay(200);
-                    nextLink?.click();
-                }
-            });
             event.preventDefault();
         }
 
@@ -124,7 +126,7 @@
         // 按下 Escape 就點擊 document.querySelector('img').click()
         if (event.key === "Escape") {
             // 如果是輸入欄位，就不要觸發
-            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA" || event.target.isContentEditable) {
+            if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
                 if (event.target.value === '' && currentPath.includes("/history")) {
                     // 只有在歷史紀錄頁面且搜尋欄位是空白時才會觸發
                     window.history.back();
@@ -134,9 +136,15 @@
                 }
                 return;
             }
-            // 找到第一個 img 元素並點擊 (Felo Logo)
-            document.querySelector('img')?.click();
-            event.preventDefault();
+
+            let backdropBlur = document.querySelector('div.backdrop-blur-md');
+            if (!backdropBlur) {
+                // 找到第一個 img 元素並點擊 (Felo Logo)
+                document.querySelector('img')?.click();
+                event.preventDefault();
+            } else {
+                backdropBlur?.parentElement?.querySelector('button')?.click();
+            }
         }
 
         // 按下 Alt+/ 就先找出所有 button 元素，比對元素內容，如果為「歷史紀錄」就點擊它
