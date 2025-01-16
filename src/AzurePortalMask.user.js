8b9e402829ccb38e761ee635bb88d8d9491226ff
Will <doggy.huang@gmail.com>
2025-01-17 01:23:35 +0800

    feat(AzurePortalMask): 更新版本至 0.2.0，新增對 azure.net 的支援並優化隱碼處理功能

--- src/AzurePortalMask.user.js
+++ src/AzurePortalMask.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Azure Portal: 將所有敏感資料進行隱碼處理
-// @version      0.1.0
+// @version      0.2.0
 // @description  移除在 Azure Portal 之中所有敏感資訊
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -10,6 +10,7 @@
 // @namespace    https://github.com/doggy8088/TampermonkeyUserscripts/raw/main/src/AzurePortalMask.user.js
 // @author       Will Huang
 // @match        *://*.portal.azure.com/*
+// @match        *://*.portal.azure.net/*
 // @match        *://portal.azure.com/*
 // @match        *://functions.azure.com/*
 // @match        *://portal.azure.us/*
@@ -18,6 +19,7 @@
 // @match        *://ms-adf.azure.com/*
 // @match        *://portal.azure.cn/*
 // @run-at       document-idle
+// @grant        GM_addStyle
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=portal.azure.com
 // ==/UserScript==
 
@@ -53,83 +55,51 @@ div.fxs-avatarmenu-tenant {
     const tagNamesToMatch = ['DIV']; // uppercase
 
     // add CSS style to blur
-    const style = document.createElement('style');
-    style.appendChild(document.createTextNode(''));
-    document.head.appendChild(style);
-
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .azdev-sensitive { ${blurCss} }`
+    GM_addStyle(
+            `.${maskEnabledClassName} .${sensitiveDataClassName} { ${blurCss} }\n` +
+            `.${maskEnabledClassName} .fxs-avatarmenu-username { display: none }\n` +
+            `.${maskEnabledClassName} input.azc-bg-light { ${blurCss} }\n` +
+            `.${maskEnabledClassName} a.fxs-topbar-reportbug { display:none; }\n` +
+            `.${maskEnabledClassName} div.fxs-topbar-internal { display:none; }\n` +
+            `.${maskEnabledClassName} .fxs-mecontrol-flyout { ${blurCss} }\n` +
+            `.${maskEnabledClassName} #mectrl_currentAccount_secondary { ${blurCss} }\n` +
+            `.${maskEnabledClassName} .fxs-avatarmenu-tenant-container { ${blurCss} }\n` +
+            `.${maskEnabledClassName} .fxs-avatarmenu-tenant-image { display:none; }\n` +
+            `.${maskEnabledClassName} .fxs-avatarmenu-tenant-image-container::after {
+                                          content: "";
+                                          display: inline-block;
+                                          background: url(https://portal.azure.com/Content/static/MsPortalImpl/AvatarMenu/AvatarMenu_defaultAvatarSmall.png) no-repeat;
+                                          width: 28px;
+                                          height: 28px;
+                                          border-radius: 28px;
+                                      }\n` +
+            `.${maskEnabledClassName} textarea.bg-white { ${blurCss} }\n` +
+            `.${maskEnabledClassName} span.qna-cs-user-id { display: none }\n` +
+            `.${maskEnabledClassName} div.directory-list-element-id { ${blurCss} }\n` +
+            `.${maskEnabledClassName} .userEmail { display:none; }\n` +
+            `.${maskEnabledClassName} .user-email { ${blurCss} }\n`
     );
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .fxs-avatarmenu-username { display: none }`
-    ); // hide name instead of blurring
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} input.azc-bg-light { ${blurCss} }`
-    ); // input boxes used for keys, connection strings, etc
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} a.fxs-topbar-reportbug { display:none; }`
-    ); // report a bug button (MS internal only)
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} div.fxs-topbar-internal { display:none; }`
-    ); // "Preview" element in top navigation bar (MS internal only)
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .fxs-mecontrol-flyout { ${blurCss} }`
-    ); // user account menu
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} #mectrl_currentAccount_secondary { ${blurCss} }`
-    ); // user account dropdown email address
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .fxs-avatarmenu-tenant-container { ${blurCss} }`
-    ); // user avatar
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .fxs-avatarmenu-tenant-image { display:none; }`
-    ); // user avatar
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .fxs-avatarmenu-tenant-image-container::after {
-        content: "";
-        display: inline-block;
-        background: url(https://portal.azure.com/Content/static/MsPortalImpl/AvatarMenu/AvatarMenu_defaultAvatarSmall.png) no-repeat;
-        width: 28px;
-        height: 28px;
-        border-radius: 28px;
-      }`
-    ); // replacement avatar
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} textarea.bg-white { ${blurCss} }`
-    ); // deployment box in (QnA maker portal)
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} span.qna-cs-user-id { display: none }`
-    ); // hide user id in profile side menu (QnA maker portal)
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} div.directory-list-element-id { ${blurCss} }`
-    ); // profile side menu in (QnA maker portal)
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .userEmail { display:none; }`
-    ); // hide name instead of blurring (ADF)
-    style.sheet.insertRule(
-        `.${maskEnabledClassName} .user-email { ${blurCss} }`
-    ); // user account dropdown email address (ADF)
 
     document.body.classList.add(maskEnabledClassName);
 
     // add class to elements already on the screen
     Array.from(document.querySelectorAll(tagNamesToMatch.join()))
-        .filter(e => shouldCheckContent(e) && sensitiveDataRegex.test(e.textContent))
+        .filter(e => shouldCheckContent(e) && (sensitiveDataRegex.test(e.textContent) || sensitiveDataRegex.test(e.childNodes[0]?.nodeValue)))
         .forEach(e => e.classList.add(sensitiveDataClassName));
 
     // add class to elements that are added to DOM later
     const observer = new MutationObserver(mutations => {
         mutations
-            .filter(
-                m =>
-                    shouldCheckContent(m.target, m.type) &&
-                    sensitiveDataRegex.test(m.target.textContent.trim())
-            )
+            .filter(m => shouldCheckContent(m.target, m.type))
             .forEach(m => {
                 const node = m.type === 'characterData' ? m.target.parentNode : m.target;
-                if (node.classList) {
-                    node.classList.add('azdev-sensitive');
-                }
+
+                Array.from(node.querySelectorAll(tagNamesToMatch.join()))
+                    .filter(n => n.classList && sensitiveDataRegex.test(n.textContent.trim()) || sensitiveDataRegex.test(n.childNodes[0]?.nodeValue))
+                    .forEach(e => {
+                        // console.log('Sensitive data found:', e);
+                        e.classList.add(sensitiveDataClassName);
+                    });
             });
     });
 
@@ -142,10 +112,11 @@ div.fxs-avatarmenu-tenant {
     observer.observe(document.body, config);
 
     function shouldCheckContent(target, mutationType) {
-        return (
-            mutationType === 'characterData' ||
-            (target && tagNamesToMatch.some(tn => tn === target.tagName))
-        );
+        // console.log('target', target, 'mutationType', mutationType)
+        let isDIV = (target && tagNamesToMatch.some(tn => tn === target.tagName))
+        let result = (mutationType === 'characterData' || isDIV);
+        // console.log('shouldCheckContent', result);
+        return result;
     }
 
 })();
