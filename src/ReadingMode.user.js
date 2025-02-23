132a36bb2498b7d252c9a901df9e077fc501c9be
Will <doggy.huang@gmail.com>
2025-02-24 00:57:20 +0800

    feat(ReadingMode): 更新版本至 0.2.2，改善內容顯示邏輯並新增對 article 和特定 div 的支援

--- src/ReadingMode.user.js
+++ src/ReadingMode.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ReadingMode: 讓網頁更容易閱讀與翻譯的工具
-// @version      0.2.1
+// @version      0.2.2
 // @description  按下 f 鍵可讓網頁僅顯示 main 元素的內容，再按一次 f 或按下 Esc 恢復原狀
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -17,26 +17,38 @@
 // @exclude      https://facebook.com/*
 // @exclude      https://www.facebook.com/*
 // @exclude      https://www.youtube.com/*
-// @exclude      https:///*
+// @exclude      https://studio.youtube.com/*
 // ==/UserScript==
 
 (function () {
-    let isMainOnly = false;
+    let isReading = false;
     let container = null;
 
     document.addEventListener('keydown', e => {
         if (isInInputMode(e.target) || e.altKey || e.metaKey || e.ctrlKey || e.shiftKey) return;
 
-        if (e.key === 'f' || e.key === 'F' || (isMainOnly && e.key === 'Escape')) {
-            isMainOnly = !isMainOnly;
-            if (isMainOnly) {
+        if (e.key === 'f' || e.key === 'F' || (isReading && e.key === 'Escape')) {
+            // console.log('isReading: ', isReading)
+            if (!isReading) {
                 // 第一次按下快速鍵時才建立暫存容器
                 if (!container) {
                     container = document.createElement('div');
                     container.style.display = 'none';
                     document.body.parentNode.appendChild(container);
                 }
-                const main = document.querySelector('main');
+                let main = document.querySelector('main');
+
+                // 如果沒有 main 元素，就找 article 元素 (必須只有一個)
+                if (!main && document.querySelectorAll('article').length === 1) {
+                    main = document.querySelector('article');
+                }
+
+                // 如果沒有 main 或 article 元素，就找第一個 div 元素
+                if (!main && location.hostname === 'marketplace.visualstudio.com') {
+                    main = document.querySelector('div.main-content');
+                }
+
+                // console.log(main)
                 if (main) {
                     // 保存所有子元素到暫存容器
                     while (document.body.firstChild) {
@@ -45,6 +57,8 @@
                     // 只顯示 main 元素
                     const mainClone = main.cloneNode(true);
                     document.body.appendChild(mainClone);
+
+                    isReading = !isReading;
                 }
             } else {
                 if (container) {
@@ -57,6 +71,8 @@
                     // 移除暫存容器
                     container.remove();
                     container = null;
+
+                    isReading = !isReading;
                 }
             }
         }
