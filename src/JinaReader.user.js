d8b303278b842c7e58ee2e1a9651cd77e8cf7eed
Will <doggy.huang@gmail.com>
2025-12-07 17:53:02 +0800

    feat(JinaReader): 更新版本至 0.2.1，簡化按鍵監聽器邏輯

--- src/JinaReader.user.js
+++ src/JinaReader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Jina Reader: 將現有網頁轉成 Markdown 格式 (alt+r)
-// @version      0.2.0
+// @version      0.2.1
 // @description  將現有網頁轉成 Markdown 格式
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
@@ -16,12 +16,8 @@
 (function () {
     'use strict';
 
-    // 為整個文檔添加按鍵監聽器
     document.addEventListener("keydown", (event) => {
-        // 判斷是否按下 Ctrl + Delete
-        if ((event.metaKey && event.key === "r")
-         || (event.altKey && event.key === "r")) {
-
+        if ((event.metaKey && event.key === "r") || (event.altKey && event.key === "r")) {
             // https://github.com/jina-ai/reader
             // https://jina.ai/reader
             location.href = 'https://r.jina.ai/' + location.href;
